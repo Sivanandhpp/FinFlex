178c1aedfc38bb89429835b599faa7a19ce67d68
sivanandhpp <sivanandhpp@gmail.com>
2024-12-04 09:23:24 +0530

    deleted file

--- lib/core/globalvalues/device_details.dart
+++
@@ -1,9 +0,0 @@
-// class DeviceDetails {
-//   // First get the FlutterView.
-// FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-
-// // Dimensions in physical pixels (px)
-// Size size = view.physicalSize;
-// double width = size.width;
-// double height = size.height;
-// }
